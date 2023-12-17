55201b72702427bf9f1cc97a32f6d8b5e85554a8
Messpelt <messpelt@gmail.com>
2023-12-17 20:46:43 +0500

    AddLocality

--- PIS GrpcService/DataAccess/ApplicationContext.cs
+++ PIS GrpcService/DataAccess/ApplicationContext.cs
@@ -10,8 +10,8 @@ public class ApplicationContext : DbContext
     {
         Database.EnsureCreated();
     }
-    public DbSet<Locality> Locality { get; set; } = default!;
-    public DbSet<LocalityCost> LocalityCosts { get; set; } = default!;
+    //public DbSet<Locality> Localities { get; set; } = default!;
+    //public DbSet<LocalityCost> LocalityCosts { get; set; } = default!;
     public DbSet<Organization> Organizations { get; set; } = default!;
     //public DbSet<Animal> Animals { get; set; } = default!;
     //public DbSet<CaptureAct> Acts { get; set; } = default!;
