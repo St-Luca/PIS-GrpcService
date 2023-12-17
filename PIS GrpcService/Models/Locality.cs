bea85cabd2c775e85ee5f385854d3019a875170d
Messpelt <messpelt@gmail.com>
2023-12-17 20:42:51 +0500

    AddLocality\Cost

---
+++ PIS GrpcService/Models/Locality.cs
@@ -0,0 +1,8 @@
+﻿namespace PIS_GrpcService.Models
+{
+    public class Locality
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ PIS GrpcService/Models/LocalityCost.cs
@@ -0,0 +1,9 @@
+﻿namespace PIS_GrpcService.Models
+{
+    public class LocalityCost
+    {
+        public int Id_Contract { get; set; }
+        public int Id_Locality { get; set; }
+        public string Cost { get; set; } = string.Empty;
+    }
+}
