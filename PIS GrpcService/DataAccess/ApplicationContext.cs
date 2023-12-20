42fada745f63df8abdd830d4d5941f6f751f911f
Messpelt <messpelt@gmail.com>
2023-12-20 23:40:57 +0500

    TestMerge

--- PIS GrpcService/DataAccess/ApplicationContext.cs
+++ PIS GrpcService/DataAccess/ApplicationContext.cs
@@ -10,12 +10,11 @@ public class ApplicationContext : DbContext
     {
         Database.EnsureCreated();
     }
-    public DbSet<Locality> Localities { get; set; } = default!; 
+    public DbSet<Locality> Localities { get; set; } = default!;
     public DbSet<LocalityCost> LocalityCosts { get; set; } = default!;
     public DbSet<Organization> Organizations { get; set; } = default!;
     public DbSet<Animal> Animals { get; set; } = default!;
     //public DbSet<CaptureAct> Acts { get; set; } = default!;
-    //public DbSet<Contract> Contracts { get; set; } = default!;
     public DbSet<Application> Applications { get; set; } = default!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,6 +29,7 @@ public class ApplicationContext : DbContext
 
         modelBuilder.Entity<Organization>().HasData(o1, o2);
 
+
         Locality loc1 = new Locality { Id = 1, Name = "Тюмень"};
         modelBuilder.Entity<Locality>().HasData(loc1);
 
@@ -39,7 +39,7 @@ public class ApplicationContext : DbContext
         Animal animal1 = new Animal { Id = 1, Category = "Собака", Sex = "Кобель", Breed = "Овчарка", Size = "Большая", Coat = "Густая", Color = "Коричневая", Ears = "Коричневая", Tail = "Короткий", CapturedAct = "1", Mark = "134", IdentChip = "192"};
         modelBuilder.Entity<Animal>().HasData(animal1);
 
-        Application app1 = new Application { Id = 1, Date = DateTime.UtcNow, ApplicantCategory = "Категория заявителя", AnimalDescription = "Коричневая овцарка", Urgency = "14", Locality = "1", Organization = "1" };
+        Application app1 = new Application { Id = 1, AnimalDescription = "Gtc", Date = DateTime.UtcNow, ApplicantCategory = "app cat", Locality = "loc", Organization = "org", Urgency = "urg" };
         modelBuilder.Entity<Application>().HasData(app1);
         //здесь прописываем связи сущностей и первоначальные данные
         //подключеине к бд через файл эппсеттингс
